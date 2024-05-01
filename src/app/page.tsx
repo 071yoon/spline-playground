14e31dcf04a4a1611480de80e3d8636bf6ee66a9
071yoon <yeonggilol@gmail.com>
2024-05-01 15:56:02 +0900

    feat: deploy simple logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import Spline from "@splinetool/react-spline";
 import "./normalize.css";
 
 export default function Home() {
-  return <main>hi</main>;
+  return (
+    <main>
+      hi
+      <Spline scene="https://prod.spline.design/GxnUCewyMBbRcQcx/scene.splinecode" />
+    </main>
+  );
 }
