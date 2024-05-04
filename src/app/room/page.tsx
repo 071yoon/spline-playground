2053d71e065ee26cceb43ad95f18b61345d91cb9
071yoon <yeonggilol@gmail.com>
2024-05-04 20:47:38 +0900

    feat: add mini room

---
+++ src/app/room/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function Page() {
+  return (
+    <>
+      <Spline scene="https://my.spline.design/miniroommusiccopy-2a5d48a87e6ec622098459aaa682153c/" />
+    </>
+  );
+}
